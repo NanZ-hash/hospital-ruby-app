1d20d55cc395f4542e6c5ac68ee44571fd05ccb8
NanZ-hash <afnanxmt@gmail.com>
2020-03-31 03:12:22 +0300

    updating the patients record

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -11,16 +11,34 @@ class PatientsController < ApplicationController
         @patient = Patient.new
     end
 
+    def edit
+        @patient=Patient.find(params[:id])
+    end 
+
     def create
-        @patient = Patient.new(patient_prams)
+        @patient = Patient.new(patient_params)
         #:patient is the modle that we difined to save it in the new record 
         # in the data base ... 
         @patient.save
         redirect_to @patient
     end
 
+    def update 
+        @patient=Patient.find(params[:id])
+        
+         @patient.update(patient_params)
+        redirect_to @patient
+
+    end
+
+        def destroy
+            @patient =Patient.find(params[:id])
+            @patient.destroy
+            redirect_to root_path
+        end
+        
     private 
-    def patient_prams
+    def patient_params
         params.require(:patient).permit(:first_name , :last_name , :diagnosis )
     end
     
